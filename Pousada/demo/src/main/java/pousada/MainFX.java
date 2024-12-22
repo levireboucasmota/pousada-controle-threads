93af3a1e57a1fafdf0310ccfac931adf0b748246
Paulo victor Cosmo <pvictorcosmo@gmail.com>
2024-12-22 18:35:03 -0300

    chore: add .jar file in git project and change pom.xml dependencies to java 21

---
+++ Pousada/demo/src/main/java/pousada/MainFX.java
@@ -0,0 +1,44 @@
+package pousada;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainFX extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        // Define the path to the FXML file for the main view of the application
+        System.out.println(getClass().getResource("/pousada/MainView.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/pousada/MainView.fxml"));
+
+        // Load the FXML file and get the root node of the scene graph
+        Parent root = loader.load();
+
+        // Get the controller instance from the FXMLLoader
+
+        // Create a new scene with the loaded root node and set it to the primary stage
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+
+        // Set the title of the application window
+        primaryStage.setTitle("Problema da televisão");
+
+        // Set the size of the window (width and height)
+        primaryStage.setWidth(1280);
+        primaryStage.setHeight(720);
+
+        // Disable resizing of the window
+        primaryStage.setResizable(false);
+
+        // Display the primary stage (the main application window)
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
